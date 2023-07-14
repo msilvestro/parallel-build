d9f41e7a9c05ff7270e8a08e907885bd9c94ef4a
Matteo Silvestro <matteosilvestro@live.it>
2023-07-15 00:37:24 +0200

    Adapt post build actions for other targets
    Support single file builds (such as Windows one) and channels for Itch.

--- parallel_build/post_build.py
+++ parallel_build/post_build.py
@@ -1,23 +1,34 @@
 import shutil
 import subprocess
+from pathlib import Path
 
 from parallel_build.config import ProjectPostBuildAction
 
 
 def execute_action(action: ProjectPostBuildAction, build_path: str):
+    build_path = Path(build_path)
+    if build_path.is_file():
+        build_path = build_path.parent
     if action.action == "copy":
         copy_build(build_path, action.params["target"])
     elif action.action == "publish-itch":
-        publish_itch(build_path, action.params["itch_user"], action.params["itch_game"])
+        publish_itch(
+            build_path,
+            action.params["itch_user"],
+            action.params["itch_game"],
+            action.params["itch_channel"],
+        )
 
 
-def copy_build(build_path: str, target_path: str):
+def copy_build(build_path: Path, target_path: str):
+    target_path = Path(target_path)
+    target_path.mkdir(exist_ok=True, parents=True)
     print(f"\n== Post build: copy build from {build_path} to {target_path}")
     shutil.copytree(build_path, target_path, dirs_exist_ok=True)
 
 
-def publish_itch(build_path: str, itch_user: str, itch_game: str):
-    itch_path = f"{itch_user}/{itch_game}:webgl"
+def publish_itch(build_path: Path, itch_user: str, itch_game: str, itch_channel: str):
+    itch_path = f"{itch_user}/{itch_game}:{itch_channel}"
     print(f"\n== Post buid: publishing to itch ({itch_path})...")
     try:
         subprocess.run(["butler", "push", build_path, itch_path])
