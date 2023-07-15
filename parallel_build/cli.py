185813da3e370b823950ee55ae42c419c82cb046
Matteo Silvestro <matteosilvestro@live.it>
2023-07-15 16:30:37 +0200

    Add basic working UI with Qt

--- parallel_build/cli.py
+++ parallel_build/cli.py
@@ -3,6 +3,7 @@ import click
 from parallel_build.build_cli import build
 from parallel_build.check_cli import check
 from parallel_build.config_cli import config
+from parallel_build.gui.cli import gui
 
 
 @click.group()
@@ -13,3 +14,4 @@ def cli():
 cli.add_command(build)
 cli.add_command(check)
 cli.add_command(config)
+cli.add_command(gui)
